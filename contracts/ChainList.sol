43b58caf678b102ce0121181f13c2e4a985ad02d
tchame44 <chamot.antoine@gmail.com>
2019-05-13 17:25:40 +0200

    add buy article

--- contracts/ChainList.sol
+++ contracts/ChainList.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.18;
 contract ChainList {
   // state variables
   address seller;
+  address buyer;
   string name;
   string description;
   uint256 price;
@@ -10,6 +11,13 @@ contract ChainList {
 //Events
 event LogSellArticle(
   address indexed _seller,
+  string _name,
+  uint256 _price
+);
+
+event LogBuyArticle(
+  address indexed _seller,
+  address indexed _buyer,
   string _name,
   uint256 _price
 );
@@ -27,9 +35,28 @@ function sellArticle(string _name, string _description, uint256 _price) public {
 //get an article
 function getArticle() public view returns (
   address _seller,
+  address _buyer,
   string _name,
   string _description,
   uint256 _price ) {
-    return(seller,name,description,price);
+    return(seller,buyer,name,description,price);
   }
+
+//buy an article
+function buyArticle() payable public {
+  //we check whether there is an article for sale
+  require(seller != 0x0);
+  //we check that the article has not been sold yet
+  require(buyer == 0x0);
+  //we dont allow the seller to buy its own article
+  require(msg.sender != seller);
+  //we check that the value sent correcspond to the price of the article
+  require(msg.value == price);
+  //keep track of the buyer's information
+  buyer=msg.sender;
+  //the buyer can pay the seller
+  seller.transfer(msg.value);
+  //trigger the events
+  LogBuyArticle(seller,buyer,name,price);
+}
 }
